efb641d3618904e8c6946c565410063096bef2e4
Alex <yatsikxvii@gmail.com>
2021-07-02 21:51:50 +0300

    gulp is not working

--- gulpfile.js
+++ gulpfile.js
@@ -9,28 +9,25 @@ gulp.task('server', function() {
 
     browserSync ({
         server: {
-            baseDir: "project_BEM"
+            baseDir: "src"
         }
     });
 
-    gulp.watch("project_BEM/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("project_BEM/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("project_BEM/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("project_BEM/sass/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
     
 });
 
