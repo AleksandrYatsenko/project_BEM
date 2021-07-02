a6c1334c84b52e8956d4483138817e9d8c1af121
Alex <yatsikxvii@gmail.com>
2021-07-02 17:02:03 +0300

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "project_BEM"
+        }
+    });
+
+    gulp.watch("project_BEM/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("project_BEM/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("project_BEM/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("project_BEM/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
