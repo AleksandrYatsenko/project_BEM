e0cb23e96d4c50efd1021a28a77a1297cbd1259f
Alex <yatsikxvii@gmail.com>
2021-11-15 09:14:28 +0200

    add wow.min.js and it working

--- js/script.js
+++ js/script.js
@@ -21,22 +21,6 @@ $(document).ready(function(){
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
-  // $('.catalog-item__link').each(function(i) {
-  //   $(this).on('click', function(e) {
-  //     e.preventDefault();
-  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-  //   });
-  // });
-
-  // $('.catalog-item__back').each(function(i) {
-  //   $(this).on('click', function(e) {
-  //     e.preventDefault();
-  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-  //   });
-  // });
-
   function toggleSlide(item) {
     $(item).each(function(i) {
       $(this).on('click', function(e) {
@@ -132,6 +116,6 @@ $(document).ready(function(){
     return false;
   });
 
-  //new WOW().init();
+  new WOW().init();
 
 });
