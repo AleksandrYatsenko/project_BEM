8ae3824e582b840b60afdf8f584741d71da228d5
Alex <yatsikxvii@gmail.com>
2021-11-16 12:15:23 +0200

    optimization after speedtest

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
+//const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "../project_BEM"
+            baseDir: "../dist"
         }
     });
 
@@ -22,13 +24,44 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("../project_BEM/css"))
+        .pipe(gulp.dest("../dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("../project_BEM/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
-    
+    gulp.watch("../project_BEM/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+    gulp.watch("../project_BEM/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function() {
+    return gulp.src("../project_BEM/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("../dist/"));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("../project_BEM/js/**/*.js")
+        .pipe(gulp.dest("../dist/js"));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("../project_BEM/fonts/**/*")
+        .pipe(gulp.dest("../dist/fonts"));
+});
+
+gulp.task('icons', function() {
+    return gulp.src("../project_BEM/icons/**/*")
+        .pipe(gulp.dest("../dist/icons"));
+});
+
+gulp.task('mailer', function() {
+    return gulp.src("../project_BEM/mailer/**/*")
+        .pipe(gulp.dest("../dist/mailer"));
+});
+
+gulp.task('images', function() {
+    return gulp.src("../project_BEM/img/**/*")
+        .pipe(gulp.dest("../dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
